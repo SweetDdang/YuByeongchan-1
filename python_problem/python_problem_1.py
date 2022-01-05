8d19f638ebe383a35305658aa1ce1b584f8d8828
SweetDdang <ybc7769@nate.com>
2022-01-05 11:47:20 +0900

    baskin 3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,12 @@
 num=0
 
-a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+while True:
+    try:
+        a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+        if 0 < a <4:
+           break
+        else:
+           print("1,2,3 중 하나를 입력하세요")
+       
+    except:
+        print("정수를 입력하세요")
