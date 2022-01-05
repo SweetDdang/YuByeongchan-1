850cf0044b97eb389bcb62d50046aad61ea07aae
SweetDdang <ybc7769@nate.com>
2022-01-05 17:58:40 +0900

    baskin 8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,40 +1,27 @@
 num=0
+
+def brGame(player1, player2):
+    while True:
+        try:
+            a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            if 0 < a <4:
+                break
+            else:
+                print("1,2,3 중 하나를 입력하세요")
+        
+        except:
+            print("정수를 입력하세요")
+
+    for i in range(a):
+        global num
+        num+=1
+        print(player1+":"+str(num))
+
+        if num == 31:
+            print(player2+" win!")
+            exit()
+    
+
 while num<32:
-    while True:
-        try:
-            a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-            if 0 < a <4:
-                break
-            else:
-                print("1,2,3 중 하나를 입력하세요")
-        
-        except:
-            print("정수를 입력하세요")
-
-    for i in range(a):
-        num+=1
-        print("playerA:"+str(num))
-
-        if num == 31:
-            print("playerB win!")
-            exit()
-        
-    while True:
-        try:
-            a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-            if 0 < a <4:
-                break
-            else:
-                print("1,2,3 중 하나를 입력하세요")
-        
-        except:
-            print("정수를 입력하세요")
-
-    for i in range(a):
-        num+=1
-        print("playerB:"+str(num))
-
-        if num == 31:
-            print("playerA win!")
-            exit()
-
+    brGame("playerA","playerB")
+    brGame("playerB","playerA")
