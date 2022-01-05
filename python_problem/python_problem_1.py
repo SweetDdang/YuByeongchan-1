9af301ea525c65462021d3f1f6fbfff54c5ae03c
SweetDdang <ybc7769@nate.com>
2022-01-05 14:44:54 +0900

    baskin 5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -11,8 +11,21 @@ while True:
     except:
         print("정수를 입력하세요")
 
-num += a
-
-while num !=0:
+for i in range(a):
+    num+=1
     print("playerA:"+str(num))
-    num -= 1
+    
+while True:
+    try:
+        a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+        if 0 < a <4:
+           break
+        else:
+           print("1,2,3 중 하나를 입력하세요")
+       
+    except:
+        print("정수를 입력하세요")
+
+for i in range(a):
+    num+=1
+    print("playerA:"+str(num))
