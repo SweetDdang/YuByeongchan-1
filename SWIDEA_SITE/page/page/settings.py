06e699da07820f93ed32b8054c96c5f0e0190d9c
SweetDdang <76276519+SweetDdang@users.noreply.github.com>
2022-02-05 11:41:41 +0900

    Update settings.py

--- SWIDEA_SITE/page/page/settings.py
+++ SWIDEA_SITE/page/page/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')thlgt^3ktk)-=2lt-+rd9)=%w93n0q-x4&+i2rt8*lyw=pcj6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.37.222.145']
 
 
 # Application definition
